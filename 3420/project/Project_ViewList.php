0582179aa73f68ce758f07769311c5226eb89dae
KrisDsouza <krisissleepy12@gmail.com>
2022-04-15 01:25:18 -0400

    Added list viewing page

---
+++ 3420/project/Project_ViewList.php
@@ -0,0 +1,70 @@
+<?php
+    include 'includes/library.php';
+    session_start();
+
+    if(!isset($_SESSION['user']))
+    {
+        header("Location:Project_login.php");
+        exit();
+    }
+    $errors = array();
+
+    //get listID from url paramters
+    $listID = $_GET['listID'];
+
+    $pdo = connectDB(); //connect to the database
+    $wishlistItems = $pdo->prepare('SELECT * FROM wishlistitems WHERE wishListID = ?;'); //prepare the query to add the name and score to the database
+    $wishlistItems->execute([$listID]); //execute the prepared query
+
+    $wishlist = $pdo->prepare('SELECT * FROM wishlisttable WHERE listID = ?;');
+    $wishlist->execute([$listID]);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>All Lists&colon; Buck-et Registry &dash; Project COIS 3420H</title>
+        <link rel="stylesheet" href="styles/project_master.css" />
+        <script src="https://kit.fontawesome.com/2b1acf3db3.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+       <header>
+            <?php include "includes/header.php";?>
+            <?php include "includes/nav.php";?>
+       </header> 
+       <main>
+           <h2>Viewing list : <?=$wishlist->fetch()['title']?></h2>
+           <section id="listItems">
+               <table>
+                   <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Link</th>
+                            <th>Action</th>     
+                        </tr>
+                   </thead>
+                   <tbody>
+                    <?php $no=1 ;foreach($wishlistItems as $row):?>
+                       <tr>
+                           <td><?=$no?></td>
+                           <td><?=$row['title']?></td>
+                           <td><?=$row['description']?></td>
+                           <td><a href="<?=$row['itemLink']?>">Item Link</a></td>
+                           <td>
+                                <a href="Project_viewitem.php?itemID=<?=$row['itemID']?>" title="View List"><span class="fa-solid fa-eye" aria-hidden="true"></span> <span class="sr-only">Edit Item</span></a>
+                                <a href="" title="View List"><span class="fa-solid fa-pen-to-square" aria-hidden="true"></span> <span class="sr-only">Edit Item</span></a>
+                                <a href="" title="View List"><span class="fa-solid fa-trash" aria-hidden="true"></span> <span class="sr-only">Delete Item</span></a>
+                           </td>
+                       </tr>
+                    <?php $no++; endforeach ?>
+                   </tbody>
+               </table>
+           </section>
+       </main>
+       <?php include "includes/footer.php";?>
+    </body>
+</html>
--- 3420/project/includes/library.php
+++ 3420/project/includes/library.php
@@ -1,8 +1,5 @@
 <?php
 // Get the acutal document and webroot path for virtual directories
-$direx = explode('/', getcwd());
-define('DOCROOT', "/$direx[1]/$direx[2]/"); // /home/username/
-define('WEBROOT', "/$direx[1]/$direx[2]/$direx[3]/"); //home/username/public_html
 
 /*############################################################
 Function for connecting to the database
@@ -11,11 +8,10 @@ Function for connecting to the database
 function connectDB()
 {
     // Load configuration as an array.
-    $config = parse_ini_file(DOCROOT . "pwd/config.ini");
-    $dsn = "mysql:host=$config[domain];dbname=$config[dbname];charset=utf8mb4";
+    $dsn = "mysql:host=localhost;dbname=test;charset=utf8mb4";
 
     try {
-        $pdo = new PDO($dsn, $config['username'], $config['password'], [
+        $pdo = new PDO($dsn, "root", "", [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
