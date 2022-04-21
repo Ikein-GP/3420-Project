5da0860f15582d649577d4e17a75ac9897e91c2a
Kris D'souza <75085069+KrisDsouza@users.noreply.github.com>
2022-04-21 18:59:09 -0400

    Added disable list and fixed index.js

--- 3420/project/scripts/index.js
+++ 3420/project/scripts/index.js
@@ -1,8 +1,13 @@
-let publicButton = document.getElementById("public-view");
-let deleteButton = document.getElementById("delete");
+document.addEventListener("DOMContentLoaded", () => {
+    let publicButtons = document.querySelectorAll('#viewlists li:nth-child(6) > button:nth-child(1)');
+    console.log(publicButtons);
+    let deleteButton = document.getElementById("delete");
+    
+    publicButtons.forEach((publicButton) => {
+        publicButton.addEventListener("click", (ev) => {
+            navigator.clipboard.writeText(publicButton.value);
+            alert("Copied Public View link to clipboard!");
+        });
+    });
 
-publicButton.addEventListener("click", (ev)=> 
-{
-    navigator.clipboard.writeText(publicButton.value);
-    alert("Copied Public View link to clipboard!");
 });
